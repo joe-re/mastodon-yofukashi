de5426c70a920d32477b9b53426d985d38f6fe25
joe-re <joe.tialtngo@gmail.com>
2017-04-30 18:08:14 +0900

    persist redux store

--- js/store.js
+++ js/store.js
@@ -4,13 +4,18 @@ import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import { createStore, compose, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import rootReducer from './reducers';
 
-const createReduxStore = (preloadedState: Object) =>
-  createStore(
+const createReduxStore = (preloadedState: Object) => {
+  const store = createStore(
     rootReducer,
     preloadedState,
-    compose(applyMiddleware(thunk, promiseMiddleware(), logger)),
+    compose(applyMiddleware(thunk, promiseMiddleware(), logger), autoRehydrate()),
   );
+  persistStore(store, { storage: AsyncStorage });
+  return store;
+};
 
 export default createReduxStore({});
